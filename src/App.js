8a4e6d07e3f2c2deedb6a48cd808fb2a43c19512
TennilleW <89853068+TennilleW@users.noreply.github.com>
2022-04-05 12:24:14 -0400

    April5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Heading from "./Components/Heading.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
-      <div className="App">
-        <h1>Hello React App</h1>
+      <div>
+        <ul>
+          <li>
+            <a class="active" href="#home">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="#news">News</a>
+          </li>
+
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
       </div>
     );
   }
---
+++ src/Components/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Heading.css";
+
+function Heading() {
+  return (
+    <div classname="Heading">
+      <h1>Best New York Restaurants</h1>
+    </div>
+  );
+}
+
+export default Heading;
