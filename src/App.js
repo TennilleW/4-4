6b9bd907c0e6828fe2e4a4dd00ffbaa1af264cfc
TennilleW <89853068+TennilleW@users.noreply.github.com>
2022-04-14 12:25:17 -0400

    4/14

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import Heading from "./Components/heading.js";
 
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: []
+    };
   }
 
   render() {
@@ -27,12 +30,8 @@ class App extends Component {
           </li>
 
           <li>
-            <a href="#about">About</a>
-          </li>
-        </ul>
-
-        <Heading />
-      </div>
+            </ul>
+        <Heading />  
     );
   }
 }
---
+++ src/FlipCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./FlipCard.css";
+import FlipCard from "./Components/flipcard.js";
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="Restaurant.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
